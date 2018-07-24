bfd2e93602e2169619ed91da0cc32c7f779ed7d1
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-07-24 11:26:59 -0400

    Describe function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+export function describe(description: string, ...callbacks: Function[]){
+	let results = []
+	let failures = []
+	callbacks.forEach(callback => {
+		callback(results, failures)
+	})
+
+	console.log(results.map(r => r ? '.' : 'F').join(''))
+	console.log('Failures:')
+	failures.forEach(failure => {
+		console.log(failure)
+	})
+}
+
 export function it(description: string, callback: Function){
 	return (results: boolean[], failures: string[]) => {
 		try {
