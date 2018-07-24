41b7d1b9920997fa1f7e69b5caa2f6db8a25e012
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-07-24 11:15:57 -0400

    'it' function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export function it(description: string, callback: Function){
+	return (results: boolean[], failures: string[]) => {
+		try {
+			callback()
+			results.push(true)
+		} catch (error) {
+			results.push(false)
+			failures.push(String(error))
+		}	
+	}
 }
