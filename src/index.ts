8318bb98bd52a9e4f0e1f0ef97a29c465314ec3b
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-07-24 11:38:09 -0400

    Uses chalk

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
-export function describe(description: string, ...callbacks: Function[]){
+import chalk from 'chalk'
+
+export function describe(description: string, ...callbacks: Function[]) {
 	let results = []
 	let failures = []
 	callbacks.forEach(callback => {
 		callback(results, failures)
 	})
 
-	console.log(results.map(r => r ? '.' : 'F').join(''))
+	console.log(results
+		.map(r => r ? chalk.green('.') : chalk.red('F'))
+		.join('')
+	)
 	console.log('Failures:')
 	failures.forEach(failure => {
 		console.log(description, failure.description)
-		console.log(String(failure.error))
+		console.log(chalk.red(String(failure.error)))
 	})
 }
 
-export function it(description: string, callback: Function){
+export function it(description: string, callback: Function) {
 	return (results: boolean[], failures: object[]) => {
 		try {
 			callback()
 			results.push(true)
 		} catch (error) {
 			results.push(false)
-			failures.push({description, error})
-		}	
+			failures.push({ description, error })
+		}
 	}
 }
